57d03fbc479f384a73ebc32777bfe829b13bce26
Annekatrin Dunkel <annekatrin.dunkel@gmx.de>
2020-01-17 16:05:55 +0100

    initial commit

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Annekatrin Dunkel on 1/17/20.
+//  Copyright © 2020 Annekatrin Dunkel. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
